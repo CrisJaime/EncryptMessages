a086078f121ed0023261e16f1c9d10d07fc4873c
Cris.Jaime <64614472+beatentorta@users.noreply.github.com>
2024-12-12 17:50:33 -0600

    Cambios: - Se agregaron al archivo mainEncrypt.py el procesamiento binario de la contraseña, el convertir la misma en pixeles y el procesar el texto a encriptar

--- mainEncrypt.py
+++ mainEncrypt.py
@@ -16,20 +16,43 @@ binariPass = convertions.dec_to_bin(asciiPass)
 nameImg = "IMG\ImageTest.jpg"
 img = processImg.cargar_imagen(nameImg)
 pixels, totalPixels, ancho, alto = processImg.extraer_canales(img)
-binarys= convertions.color_to_bin(pixels,ancho,alto)
-
-print(f"[INFO] ASCII: {asciiPass}")
-print(f"[INFO] Total de Pixels: {totalPixels}")
+binarys = convertions.color_to_bin(pixels, ancho, alto)
 
 # Binary processing
 indicadores = manipulation.separar_clave(binariPass)
-binPassSep = manipulation.separar_contrasena(binariPass,keyLong)
-pixelsClaveModificada = manipulation.manipular_indicadores(binarys,indicadores)
-pixelsIndicadores= manipulation.crear_pixeles(pixelsClaveModificada)
-pixelsIndicadoresDec=convertions.bin_to_dec(pixelsIndicadores)
+binPassSep = manipulation.separar_contrasena(binariPass, keyLong)
+pixelsClaveModificada = manipulation.manipular_indicadores(
+    binarys, indicadores)
+pixelsIndicadores = manipulation.crear_pixeles(pixelsClaveModificada)
+pixelsIndicadoresDec = convertions.bin_to_dec(pixelsIndicadores)
 
+# Pixel Password
+binPassMod = manipulation.manipular_contrasena(binarys, binPassSep, ancho)
+pixelsBinContra = manipulation.crear_pixeles(binPassMod)
+pixelsBinDec = convertions.bin_to_dec(
+    pixelsBinContra)  # Pixeles Contraseña Modificados
+pixelsPassMod = pixelsIndicadoresDec + \
+    pixelsBinDec  # Total Pixeles para Contraseña
+pixels[0][0:88] = pixelsPassMod
+
+# Text processing
+textEncrypt = "Hola esto es una prueba para encriptar un mensaje en una imagen"
+textToList = list(textEncrypt)
+textASCII = convertions.convert_ascii(textToList)# Texto convertido en ASCII
+textBin =  convertions.dec_to_bin(textASCII)# Texto convertido en binario
+
+totalBitsText= len(textBin)*8# Total de bits
+totalPixelsText= round(totalBitsText/3 +1/3)
+
+print(f"[INFO] Contraseña en ASCII: {asciiPass}")
+print(f"[INFO] Total de Pixels: {totalPixels}")
 print(f"[INFO] Total de Caracteres por Fila: {len(pixels)*3//8}")
 print(f"[INFO] Total de Filas de la imagen: {alto}")
+print(f"[INFO] Total de Pixeles de la contraseña: {len(pixelsPassMod)}")
+print("---"*10)
+print(f"[INFO] Texto a encriptar: {textEncrypt}")
+print(f"[INFO] Total de bits que usara el texto a encryptar: {totalBitsText}")
+print(f"[INFO] Total de Pixeles que usara el texto a encryptar: {totalPixelsText}")
 
 fin = time.time()
 print(f"[INFO] Tiempo transcurrido: {round(fin - inicio,3)} seg")
